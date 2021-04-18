2c2a812970a37dfabfeba219e7743f62e1118fc3
Gonzalo Cercos Vazquez <gcercos@altech.es>
2021-04-18 14:16:17 +0200

    Test: A file that not exist should fail.

--- getData.spec.js
+++ getData.spec.js
@@ -45,4 +45,37 @@ describe('Fetching the data', () => {
             done();
         })
     })
+
+    it('Obtain the data from a specific TXT file', done => {
+        getData.getData('./lessData.txt').then(result => {
+            let received = result;
+            let expected = [
+                {
+                    id: 1,
+                    homeTeam: 'Mexico',
+                    awayTeam: 'Canada',
+                    homeScore: 0,
+                    awayScore: 5
+                },
+                {
+                    id: 2,
+                    homeTeam: 'Spain',
+                    awayTeam: 'Brazil',
+                    homeScore: 10,
+                    awayScore: 2
+                }
+            ];
+            expect(received).toEqual(expected);
+            done();
+        })
+    })
+
+    it('Throw a file not found error', done => {
+        getData.getData('./nonExistingFile.txt').then(result => {
+            let received = result;
+            let expected = new Error("The file was not found");
+            expect(received).toEqual(expected);
+            done();
+        })
+    })
 })
