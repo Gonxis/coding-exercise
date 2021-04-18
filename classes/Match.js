d84b42ba16e89ff26b4101b9eeb0d8d3f5c60977
Gonzalo Cercos Vazquez <gcercos@altech.es>
2021-04-18 15:20:01 +0200

    Feat: Creating class Match with startMatch

---
+++ classes/Match.js
@@ -0,0 +1,19 @@
+module.exports = class Match {
+    constructor(homeTeam, awayTeam, homeScore = 0, awayScore = 0, inProgress = false) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = homeScore;
+        this.awayScore = awayScore;
+        this.inProgress = inProgress;
+    }
+
+    startMatch() {
+        return {
+            homeTeam: this.homeTeam,
+            awayTeam: this.awayTeam,
+            homeScore: 0,
+            awayScore: 0,
+            inProgress: true
+        }
+    }
+}
