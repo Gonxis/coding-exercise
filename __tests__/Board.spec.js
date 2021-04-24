95cbf2977d8afa56e6415a75ee409deaa957cf37
Gonzalo Cercos Vazquez <gcercos@altech.es>
2021-04-24 19:32:10 +0200

    Test: Get summary games by total score

--- __tests__/Board.spec.js
+++ __tests__/Board.spec.js
@@ -8,4 +8,47 @@ describe('Class Board tests', () => {
 
         expect(scoreBoard.matchList).toEqual(matchList.default);
     });
+
+    it('Get summary games by total score and if they are with same score, first the last one in the system', () => {
+        const scoreBoard = new Board();
+        const received = scoreBoard.getSummaryGamesByTotalScore();
+        const expected = [
+            {
+                "id": 4,
+                "homeTeam": "Uruguay",
+                "awayTeam": "Italy",
+                "homeScore": 6,
+                "awayScore": 6
+            },
+            {
+                "id": 2,
+                "homeTeam": "Spain",
+                "awayTeam": "Brazil",
+                "homeScore": 10,
+                "awayScore": 2
+            },
+            {
+                "id": 1,
+                "homeTeam": "Mexico",
+                "awayTeam": "Canada",
+                "homeScore": 0,
+                "awayScore": 5
+            },
+            {
+                "id": 5,
+                "homeTeam": "Argentina",
+                "awayTeam": "Australia",
+                "homeScore": 3,
+                "awayScore": 1
+            },
+            {
+                "id": 3,
+                "homeTeam": "Germany",
+                "awayTeam": "France",
+                "homeScore": 2,
+                "awayScore": 2
+            },
+        ];
+        expect(received).toEqual(expected);
+    })
 })
