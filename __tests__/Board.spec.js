2fee3f844923d7eaf15d52b1674c83a3c66332fe
Gonzalo Cercos Vazquez <gcercos@altech.es>
2021-04-24 19:21:22 +0200

    Feat: Implementing Board contructor

--- __tests__/Board.spec.js
+++ __tests__/Board.spec.js
@@ -4,47 +4,8 @@ import * as matchList from '../data.json';
 
 describe('Class Board tests', () => {
     it('Board constructor', () => {
-        const scoreBoard = new Board(matchList);
-        const expected = {
-            "matches": [
-                {
-                    "id": 1,
-                    "homeTeam": "Mexico",
-                    "awayTeam": "Canada",
-                    "homeScore": 0,
-                    "awayScore": 5
-                },
-                {
-                    "id": 2,
-                    "homeTeam": "Spain",
-                    "awayTeam": "Brazil",
-                    "homeScore": 10,
-                    "awayScore": 2
-                },
-                {
-                    "id": 3,
-                    "homeTeam": "Germany",
-                    "awayTeam": "France",
-                    "homeScore": 2,
-                    "awayScore": 2
-                },
-                {
-                    "id": 4,
-                    "homeTeam": "Uruguay",
-                    "awayTeam": "Italy",
-                    "homeScore": 6,
-                    "awayScore": 6
-                },
-                {
-                    "id": 5,
-                    "homeTeam": "Argentina",
-                    "awayTeam": "Australia",
-                    "homeScore": 3,
-                    "awayScore": 1
-                }
-            ]
-        };
+        const scoreBoard = new Board();
 
-        expect(scoreBoard.matchList).toEqual(expected);
+        expect(scoreBoard.matchList).toEqual(matchList.default);
     });
 })
--- classes/Board.js
+++ classes/Board.js
@@ -1,8 +1,10 @@
 const Match = require('./Match');
 const game1 = new Match("team 1", "team 2");
+const getData = require('../getData');
+const allMatchesInAnObj = require('../data.json');
 
 module.exports = class Board {
-    constructor(constructorProp) {
-        this.constructorProp = constructorProp;
+    constructor() {
+        this.matchList = allMatchesInAnObj;
     }
 }
