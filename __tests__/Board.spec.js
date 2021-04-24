2a9fa787e45a33197397214ad56ab6fe30b53c20
Gonzalo Cercos Vazquez <gcercos@altech.es>
2021-04-24 19:18:27 +0200

    Test: Board constructor test

--- __tests__/Board.spec.js
+++ __tests__/Board.spec.js
@@ -1,9 +1,50 @@
 import Board from '../classes/Board';
 import Match from '../classes/Match';
+import * as matchList from '../data.json';
 
 describe('Class Board tests', () => {
     it('Board constructor', () => {
-        const scoreBoard = new Board("something");
-        expect(scoreBoard.constructorProp).toBe("something");
+        const scoreBoard = new Board(matchList);
+        const expected = {
+            "matches": [
+                {
+                    "id": 1,
+                    "homeTeam": "Mexico",
+                    "awayTeam": "Canada",
+                    "homeScore": 0,
+                    "awayScore": 5
+                },
+                {
+                    "id": 2,
+                    "homeTeam": "Spain",
+                    "awayTeam": "Brazil",
+                    "homeScore": 10,
+                    "awayScore": 2
+                },
+                {
+                    "id": 3,
+                    "homeTeam": "Germany",
+                    "awayTeam": "France",
+                    "homeScore": 2,
+                    "awayScore": 2
+                },
+                {
+                    "id": 4,
+                    "homeTeam": "Uruguay",
+                    "awayTeam": "Italy",
+                    "homeScore": 6,
+                    "awayScore": 6
+                },
+                {
+                    "id": 5,
+                    "homeTeam": "Argentina",
+                    "awayTeam": "Australia",
+                    "homeScore": 3,
+                    "awayScore": 1
+                }
+            ]
+        };
+
+        expect(scoreBoard.matchList).toEqual(expected);
     });
 })
